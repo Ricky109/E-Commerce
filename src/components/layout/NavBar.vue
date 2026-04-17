bf2bb59283557764743394a0917f0e27c50ca988
Dayana Coaguila <142510427+DayanaCoaguila@users.noreply.github.com>
2026-04-16 22:54:32 -0500

    Modificaciones

--- src/components/layout/NavBar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <nav>
-    <RouterLink to="/">Inicio</RouterLink>
-    <RouterLink to="/products">Catálogo</RouterLink>
-    <RouterLink to="/cart">Carrito</RouterLink>
-    <RouterLink to="/about">Nosotros</RouterLink>
-  </nav>
-</template>
